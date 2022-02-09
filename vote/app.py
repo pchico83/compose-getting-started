e9e90c9ce46ce4ea4ae0876b47f91b6106bedaf9
Pablo Chico de Guzman <pchico83@gmail.com>
2022-02-09 15:07:49 +0100

    Add host volumes

--- vote/app.py
+++ vote/app.py
@@ -13,8 +13,8 @@ redis = Redis(host="redis", db=0)
 app = Flask(__name__)
 
 def getOptions():
-    option_a = "Cats"
-    option_b = "Dogs"
+    option_a = "Burritos"
+    option_b = "Tacos"
     return option_a, option_b
 
 @app.route("/", methods=['POST','GET'])
